f52dbcc36a63c2369abcdabb9fe4581b7273abe4
Gonzalo La Ferrera <charan@PC-cita>
2023-11-03 19:47:23 -0300

    Proyecto Finalizado con Login-Logout-Register en Firebase

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,21 @@
+import { logout } from "../config/firebase";
+
 const Dashboard = () => {
-    return "Dashboard";
+
+    const handleLogout = async() => {
+        try {
+            logout();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <>
+            <h1>Dashboard (ruta protegida)</h1>
+            <button onClick={handleLogout}>Logout</button>
+        </>
+    );
 };
 
 export default Dashboard;
